cc7b87d664881dc9773c33e5c95c40dba201cac6
Grisha Trubetskoy <grisha@apache.org>
2012-11-07 21:24:52 -0500

    Put the SETNX against expired key test back because it works as expected now

--- tests/unit/basic.tcl
+++ tests/unit/basic.tcl
@@ -237,29 +237,28 @@ start_server {tags {"basic"}} {
         assert_equal 10 [r get x]
     }
 
-    # THREDIS TODO this feature is actually broken
-    # test "SETNX against expired volatile key" {
-    #     # Make it very unlikely for the key this test uses to be expired by the
-    #     # active expiry cycle. This is tightly coupled to the implementation of
-    #     # active expiry and dbAdd() but currently the only way to test that
-    #     # SETNX expires a key when it should have been.
-    #     for {set x 0} {$x < 9999} {incr x} {
-    #         r setex key-$x 3600 value
-    #     }
-    #     # This will be one of 10000 expiring keys. A cycle is executed every
-    #     # 100ms, sampling 10 keys for being expired or not.  This key will be
-    #     # expired for at most 1s when we wait 2s, resulting in a total sample
-    #     # of 100 keys. The probability of the success of this test being a
-    #     # false positive is therefore approx. 1%.
-    #     r set x 10
-    #     r expire x 1
+    test "SETNX against expired volatile key" {
+        # Make it very unlikely for the key this test uses to be expired by the
+        # active expiry cycle. This is tightly coupled to the implementation of
+        # active expiry and dbAdd() but currently the only way to test that
+        # SETNX expires a key when it should have been.
+        for {set x 0} {$x < 9999} {incr x} {
+            r setex key-$x 3600 value
+        }
+        # This will be one of 10000 expiring keys. A cycle is executed every
+        # 100ms, sampling 10 keys for being expired or not.  This key will be
+        # expired for at most 1s when we wait 2s, resulting in a total sample
+        # of 100 keys. The probability of the success of this test being a
+        # false positive is therefore approx. 1%.
+        r set x 10
+        r expire x 1
 
-    #     # Wait for the key to expire
-    #     after 2000
+        # Wait for the key to expire
+        after 2000
 
-    #     assert_equal 1 [r setnx x 20]
-    #     assert_equal 20 [r get x]
-    # }
+        assert_equal 1 [r setnx x 20]
+        assert_equal 20 [r get x]
+    }
 
     test {EXISTS} {
         set res {}
